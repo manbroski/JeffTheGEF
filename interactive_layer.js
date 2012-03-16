c791556cea9c017b034623d8a96eae25cf298289
manbroski <manbroski@gmail.com>
2012-03-16 00:26:56 -0600

    Cleaned up the code a little

--- interactive_layer.js
+++ interactive_layer.js
@@ -1,63 +1,31 @@
-function start() {
-	var stage = new Kinetic.Stage("canvas", 500, 500);
-	var main_layer = new Kinetic.Layer();
-	var message_layer = new Kinetic.Layer();
+var canvasHeight = 800;
+var canvasWidth = 800;
+
+function main() {
+	var stage = new Kinetic.Stage("canvas", canvasWidth, canvasHeight);
+	var mainLayer = new Kinetic.Layer();
+	var messageLayer = new Kinetic.Layer();
 
 
-	//create caption
-	/*var caption = new Kinetic.Text({
-		x: stage.width / 2 - 25,
-		y: target_box.y +target_box.width + 20,
-		text: "Drag that glucose inside!",
-		fontSize: 12,
-		fontFamily: "Calibri",
-		textFill: "black"
-	});
-	main_layer.add(caption);*/
+	//TODO: create an automatic image loader	
 
-	var protein;
-	//draw image
-	var imageObject = new Image();
-	imageObject.onload = function (){
-		protein = new Kinetic.Image({
-			x: 100, 
-			y: 100,
-			width: 70,
-			height: 100,
-			image: imageObject,
-			stroke: "green",
-		});
-		main_layer.add(protein);
-		main_layer.add(target_box);
-		main_layer.add(glucose);
-		stage.add(main_layer);
-		stage.add(message_layer);
-		writeMessage(message_layer, "Drag that glucose in the active site!");
-	};
-	imageObject.src = "prot.jpg";
+	//create static elements
+	mainLayer.add(bilayer(250,50,30));
+	mainLayer.add(barrelProtein(200,239,40,50,"#62B869"));
 
+	//create dynamic elements
+	var glucose = glucose(10,50,10,"yellow",true);
+	var target_box = rectangle (100,100,20,20,"green",false);
 
-	//create a substrate shape
-	var glucose = new Kinetic.RegularPolygon({
-		x: 100,
-		y: 50,
-		sides: 6,
-		radius: 6,
-		stroke: "black",
-		fill: "yellow",
-		strokeWidth: 1,
-		draggable: true 
-	});
-	
-	//create drop-box with a title
-	var target_box = new Kinetic.Rect({
-		x: 100 ,
-		y: 100 ,
-		width: 10,
-		height: 10,
-		stroke: "red",
-		strokeWidth: 1,
-	});
+	//add dynamic stuff to the layers
+	mainLayer.add(target_box);
+	mainLayer.add(glucose);
+
+	//create stage
+	stage.add(mainLayer);
+	stage.add(messageLayer);
+
+	writeMessage(messageLayer, "Drag that glucose in the active site!");
 
 	//set pointer cursor for the glucose
 	glucose.on("mouseover", function(){
@@ -70,45 +38,16 @@ function start() {
 	//detect glucose being dropped
 	glucose.on("dragend", function() {
 		if (glucose.x > target_box.x && glucose.x < target_box.x + target_box.width && glucose.y > target_box.y && glucose.y < target_box.y + target_box.height) {
-			writeMessage(message_layer, "Good job!");
+			writeMessage(messageLayer, "Good job!");
 			glucose.draggable(false);
-			/*stage.onFrame(function(frame){
-				target_x = target_box.x+target_box.width/2;
-				target_y = target_box.y+target_box.height/2; 
-				while (glucose.x != target_x && glucose.y != target_y) {
-					if (glucose.x < target_x) {
-						glucose.x++;
-					} else if (glucose.x > target.x) {
-						glucose.x--;
-					}
-					if (glucose.y < target_y) {
-						glucose.y++;
-					} else if (glucose.y > target_y) {
-						glucose.y--;
-					}
-				
-					main_layer.draw();
-				}
-			});*/
 		}
-
 	});
 	
-	//draw everything
-
-	//stage.add(main_layer);
-
-	//stage.start();
-
-
-	//var debug_text = document.getElementById("debug_zone");
-	//debug_text.innerHTML=target_box.y + 20;	
-	
-	
-	//notice that messages have to be written after the message layer was created	
-
 }
 
+/**
+ * Write a message to the message layer
+ */
 function writeMessage(messageLayer, message) {
 	var context = messageLayer.getContext();
 	messageLayer.clear();
@@ -116,3 +55,106 @@ function writeMessage(messageLayer, message) {
 	context.fillStyle = "black";
 	context.fillText(message, 10, 25);
 }
+
+/**
+ * Create a simple bilayer backdrop.
+ * Solid, no graphics
+ * @param origin: origin of the bilayer drawing (upper left corner)
+ * @param thickness: height of the drawing
+ * @param curvature: inflection value for the curve. Suggested: 30
+ */
+function bilayer(origin, thickness, curvature) {
+	return new Kinetic.Shape({
+		drawFunc: function() {
+			var context = this.getContext();
+			context.beginPath();
+			context.moveTo(0, origin);
+			context.quadraticCurveTo(canvasWidth/2, origin - curvature, canvasWidth, origin);
+			context.lineTo(canvasWidth, origin + thickness);
+			context.quadraticCurveTo(canvasWidth/2,	origin + thickness - curvature , 0, origin + thickness);
+			context.closePath();
+			this.fillStroke();
+		},
+		fill:"#FFDF64"
+	});
+}
+
+/**
+ * Create a simple barrel protein drawing.
+ * Suggested to be used as a generic membrane protein shape.
+ * Gradient, with a stroke outline.
+ * @param x
+ * @param y
+ * @param width
+ * @param height: suggested to be equal to membrane thickness
+ * @param color
+ */
+
+function barrelProtein (x, y, width, height, color) {
+	var	curvature = 20;
+	return new Kinetic.Shape({
+		drawFunc: function() {
+			var context = this.getContext();
+			
+			//draw
+			context.beginPath();
+			context.moveTo(x, y);
+			context.quadraticCurveTo(x + width/2, y - curvature, x + width, y);
+			context.lineTo(x + width, y + height);
+			context.quadraticCurveTo(x + width/2, y + height + curvature , x, y + height);
+			context.closePath();
+			
+			//add gradient
+			var gradient = context.createRadialGradient(x, y, width, x + width, y + height, width);
+			gradient.addColorStop(0, "#FFDFFF");
+			gradient.addColorStop(1, "#004CB3");
+			context.fillStyle = gradient;
+			context.fill();
+			context.stroke();
+		},
+		stoke: "black",
+		strokeWidth: 1
+	});
+}
+
+/**
+ * Create a glucose shape.
+ * @param x
+ * @param y
+ * @param radius
+ * @param color
+ * @param draggable
+ */
+function glucose (x, y, radius, color, draggable) {
+	return new Kinetic.RegularPolygon({
+		x: x,
+		y: y,
+		sides: 6,
+		radius: radius,
+		stroke: "black",
+		fill: color,
+		strokeWidth: 1,
+		draggable: draggable 
+	});
+}
+
+/**
+ * Create a rectangle shape
+ * @param x
+ * @param y
+ * @param width
+ * @param height
+ * @param color
+ * @param draggable
+ */
+function rectangle (x, y, width, height, color, draggable) {
+	return new Kinetic.Rect({
+		x: x,
+		y: y,
+		width: width,
+		height: height,
+		fill: color,
+		draggable: draggable
+	});
+}
+
